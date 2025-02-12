8354b3453ced6025404fe85f7cebb5861e4ae070
Leon Edlund <Leone.edlund@gmail.com>
2025-02-12 12:58:19 +0100

    Refactoring fetch data function

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,8 @@
-const fetchData = async () => {
-  const data = await fetch("/view");
+const fetchData = async (url) => {
+  const data = await fetch(url);
   const response = await data.text();
   console.log(response);
   document.getElementById("app").innerHTML = response;
 }
 
-fetchData();
+fetchData("/view/projects.html");
